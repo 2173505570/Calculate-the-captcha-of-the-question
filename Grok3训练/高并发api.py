2d914ca0acb3e9ea64f475bb9124117b46ec3d1a
suixing_sir <2173505570@qq.com>
2025-03-11 22:30:19 +0800

    新增

--- Grok3训练/高并发api.py
+++ Grok3训练/高并发api.py
@@ -134,4 +134,4 @@ async def recognize(file: UploadFile = File(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8888)
+    uvicorn.run(app, host="0.0.0.0", port=1234)
